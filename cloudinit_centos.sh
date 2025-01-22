7dda2f45f275904d515f3861a4e20f14de46d1e8
Marek <marek@techfellow.it>
2025-01-22 18:55:55 +0100

    small update

--- cloudinit_centos.sh
+++ cloudinit_centos.sh
@@ -8,25 +8,20 @@ users:
     sudo: "ALL=(ALL) NOPASSWD:ALL"
     groups: sudo
     shell: /bin/bash
+runcmd:
+  - sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
+  - sed -i 's/mirrorlist/#mirrorlist/g'  /etc/yum.repos.d/CentOS-*
+  - dnf swap centos-{linux,stream}-repos
+  - sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
+  - sed -i 's/mirrorlist/#mirrorlist/g'  /etc/yum.repos.d/CentOS-*
+  - dnf distro-sync --best --allowerasing
 packages:
   - git
   - htop
   - curl
-runcmd:
-  - systemctl restart sshd
-  - sed -i 's/PubkeyAuthentication no/PubkeyAuthentication yes/g' /etc/ssh/sshd_config
-  - sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
-  - chown -R madmin:madmin /home/madmin/.ssh
-  - chmod 700 -R /home/madmin/.ssh
-
-# Upgrade Centos 8 to Centos 8 Stream
-sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-* && sed -i 's/mirrorlist/#mirrorlist/g'  /etc/yum.repos.d/CentOS-*
-dnf distro-sync --best --allowerasing
-sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-* && sed -i 's/mirrorlist/#mirrorlist/g'  /etc/yum.repos.d/CentOS-*
-dnf swap centos-{linux,stream}-repos
-
-# Optional - not always work
-# dnf install centos-release-stream -y --allowerasing
-# dnf swap centos-{linux,stream}-repos
-# dnf distro-sync --best --allowerasing
-# reboot
+  - wget
+  - make
+  - gcc
+  - automake
+  - autoconf
+  - nano
